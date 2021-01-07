817e0ef4f0fd61e19bc69a5586b18179da90e5bc
davidauza.engineer <davidauza01@gmail.com>
2021-01-07 09:15:57 -0500

    Update my view controller

--- app/controllers/my_view_controller.rb
+++ app/controllers/my_view_controller.rb
@@ -1,3 +1,11 @@
 class MyViewController < UIViewController
+  alias :'super_initWithUser:' :'initWithUser:'
+
+  def viewDidLoad
+    # [super viewDidLoad];
+    super
+    # [super initWithUser: user];
+    # super_initWithUser user
+  end
 
 end
