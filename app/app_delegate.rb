09862949fad06cac2c61374160a81d85de5e93eb
davidauza.engineer <davidauza01@gmail.com>
2021-01-08 08:59:46 -0500

    Use enumerations with RubyMotion

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -9,14 +9,21 @@ class AppDelegate
     # my_vc = MyViewController.alloc.initWithNibName('MyView', bundle: nil)
     # my_vc.isViewLoaded?
     # my_vc.background = background
-    dictionary = {}
-    puts dictionary.class.superclass
+    # dictionary = {}
+    # puts dictionary.class.superclass
 
-    dictionary['key']
-    dictionary['key'] = 'value'
+    # dictionary['key']
+    # dictionary['key'] = 'value'
 
-    frame = CGRect.new([100, 100], [100, 40])
-    puts frame.inspect
+    # frame = CGRect.new([100, 100], [100, 40])
+    # puts frame.inspect
+
+    # [@[@"one", @"two", @"three"] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+
+    # }]
+    ['one', 'two', 'three'].enumerateObjectsUsingBlock(lambda do |obj, idx, stop_ptr|
+      puts obj
+    end)
 
     true
   end
